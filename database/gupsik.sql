544714f8284c041886a8d947ce85ac06c94f8a5e
eggrole <bepuetv123@gmail.com>
2022-11-27 02:03:14 +0900

    데이터베이스 파일 추가

---
+++ database/gupsik.sql
@@ -0,0 +1,61 @@
+-- MariaDB dump 10.19  Distrib 10.10.2-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: gupsik
+-- ------------------------------------------------------
+-- Server version	10.10.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data` (
+  `ID` int(11) DEFAULT NULL COMMENT '열의 id',
+  `GRADE` int(11) DEFAULT NULL COMMENT '학년',
+  `CLASS` int(11) DEFAULT NULL COMMENT '반',
+  `NUM` int(11) DEFAULT NULL COMMENT '번호',
+  `NAME` varchar(50) DEFAULT NULL COMMENT '이름',
+  `UID` varchar(12) DEFAULT NULL COMMENT '카드의 UID',
+  `IS_EAT` tinyint(4) DEFAULT 0 COMMENT '먹음?'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci COMMENT='Student''s data';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data`
+--
+
+LOCK TABLES `data` WRITE;
+/*!40000 ALTER TABLE `data` DISABLE KEYS */;
+INSERT INTO `data` VALUES
+(1,1,3,3,'김동훈','99121150172',1),
+(2,1,3,1,'기록현','685322681',1),
+(3,1,3,4,'김정원','21189163172',1),
+(4,1,1,9,'김형서','312115146',1),
+(5,1,2,21,'장진우','24525783',1),
+(6,1,1,22,'이재옥','19177155172',1);
+/*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-27  2:01:41
